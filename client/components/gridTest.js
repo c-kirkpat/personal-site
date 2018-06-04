f83f01fe834125e38bedbae34343ea6e90e6bc63
Charley Kirkpatrick <ckirkpat7@gmail.com>
2018-06-04 09:48:04 -0500

    fucked the fuck up

--- client/components/gridTest.js
+++ client/components/gridTest.js
@@ -1,20 +1,30 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 /**
  * COMPONENT
  */
 export const GridTest = () => {
-  return (
-    <div className='wrapper'>
-      <h3 className='header'>CHAR</h3>
-      <h3 className='header2'>LEY</h3>
-      <h3 className='lastName'>KIRK</h3>
-      <h3 className='lastName2'>PATRICK</h3>
-      <p className='thing'>Hello this is some lorem ipsum that i manually typed out </p>
-      <p className='thing2'>Hello this is some lorem ipsum that i manually typed out </p>
-    </div>
-  )
+    return (
+        <div className='wrapper'>
+            <div className='firstNameWrapper'>
+                <div className='header'>CHAR</div>
+                <div className='header2'>LEY</div>
+            </div>
+            <div className='lastNameWrapper'>
+                <div className='lastName'>KIRK</div>
+                <div className='lastName2'>PATRICK</div>
+            </div>
+            <div className='banner'>DEVELOPER </div>
+            <p className='thing'>Hello this is some lorem ipsum that i manually typed out </p>
+            <p className='thing2'>Hello this is some lorem ipsum that i manually typed out </p>
+            <div className='diamond-narrow diamond1' />
+            <div className='diamond-narrow diamond2' />
+            <div className='diamond-narrow diamond3' />
+            <div className='diamond-narrow diamond4' />
+
+        </div>
+    )
 }
 
 /**
